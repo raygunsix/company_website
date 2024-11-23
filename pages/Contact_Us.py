1c9883affe8341fce48be112697a0fa2efa54821
Chris Reid <chris@raygun6.com>
2024-11-22 21:44:47 -0800

    complete email form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 st.set_page_config(layout="wide")
 
@@ -6,7 +7,17 @@ st.header("Contact Us")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email message: ")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+
+    # Backslash and LB required when including subject in email text
+    message = f"""\
+    Subject: New email from {user_email}
+
+    From: {user_email}
+    {raw_message}
+    """
+
     button = st.form_submit_button("Submit")
     if button:
-        print("Pressed")
+        send_email(message)
+        st.info("Your email was sent")
--- send_email.py
+++ send_email.py
@@ -1,20 +1,20 @@
 import smtplib, ssl
 
-host = "smtp.example.com"
-port = 465
 
-username = "user@example.com"
-password = "password"
+def send_email(message):
+    """ sends message via email """
+    host = "smtp.example.com"
+    port = 465
 
-receiver = "user2@example.com"
-context = ssl.create_default_contect()
+    username = "user@example.com"
+    password = "password"
 
-# Backslash required when including subject in email text
-message = """\
-Subject: Inquiry
-Hi, how are you?
-"""
+    receiver = "user2@example.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    # with smtplib.SMTP_SSL(host, port, context=context) as server:
+    #     server.login(username, password)
+    #     server.sendmail(username, receiver, message)
+
+    print(message)
+
