0ebe09c5b415030b614332f64b0e1c9cdc4821eb
Chris Reid <chris@raygun6.com>
2024-11-22 21:13:49 -0800

    add contact form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -4,3 +4,9 @@ st.set_page_config(layout="wide")
 
 st.header("Contact Us")
 
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email message: ")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("Pressed")
