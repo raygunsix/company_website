8156ad94b1406160cff74200db99d55a08007be3
Chris Reid <chris@raygun6.com>
2024-11-22 22:04:20 -0800

    add topic selection to contact form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,20 +1,30 @@
 import streamlit as st
+import pandas
 from send_email import send_email
 
 st.set_page_config(layout="wide")
 
 st.header("Contact Us")
 
+df = pandas.read_csv("topics.csv")
+
 with st.form(key="email_form"):
     user_email = st.text_input("Your email message: ")
+
+    user_topic = st.selectbox(
+        "What topic would you like to discuss?",
+        df["topic"]
+    )
+
     raw_message = st.text_area("Your message")
 
     # Backslash and LB required when including subject in email text
     message = f"""\
-    Subject: New email from {user_email}
+Subject: New email from {user_email}
 
-    From: {user_email}
-    {raw_message}
+From: {user_email}
+Topic: {user_topic}
+{raw_message}
     """
 
     button = st.form_submit_button("Submit")
